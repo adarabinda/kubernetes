9d604b06786985f55b0c19a52c96c8b2812f02e6
adarabinda <42712109+adarabinda@users.noreply.github.com>
2020-05-11 21:57:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.status(200).json('Hello world')
+  res.status(200).json('Hello world I have updated Buddy')
 })
 
 module.exports = app.listen(PORT, () => console.log(`Running on ${ PORT }`))
