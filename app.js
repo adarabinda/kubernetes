6f1ee26bf653187a70bbc6278f8b7701be49fddc
adarabinda <42712109+adarabinda@users.noreply.github.com>
2020-05-11 21:50:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 
 const app = express()
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.status(200).json('Hello world')
 })
 
-module.exports = app.listen(4000, () => console.log(`Running on http://localhost:4000`))
+module.exports = app.listen(PORT, () => console.log(`Running on ${ PORT }`))
